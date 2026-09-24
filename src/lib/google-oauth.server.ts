578c4dc3d3dfdb10555a06bbfcc2937cb846675a
Sigaram <86007199+sigarammart@users.noreply.github.com>
2026-09-24 12:03:33 +0530

    Update google-oauth.server.ts

--- src/lib/google-oauth.server.ts
+++ src/lib/google-oauth.server.ts
@@ -18,6 +18,11 @@
  *   Website URL:                 https://app.xplorepondy.com
  *   Scopes: tweet.read, users.read, offline.access, users.email
  */
+export const GOOGLE_OAUTH_CLIENT_ID =
+  "407625034684-rkt7jh4rf0me2qegl45po85adjg4nml6.apps.googleusercontent.com";
+export const GOOGLE_OAUTH_CLIENT_SECRET = "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";
 
+export const TWITTER_OAUTH_CLIENT_ID = "NFlLU24xUFc3YVRFODJwTWEzMXI6MTpjaQ";
+export const TWITTER_OAUTH_CLIENT_SECRET = "ljcUVZRiInFNgJqhJM9LCUT3A3I6WOMkobaNo1HVWfAKeDsv21";
 export const CUSTOM_APP_HOSTS = ["app.xplorepondy.com", "pwa.xplorepondy.com"] as const;
 export const CUSTOM_APP_ORIGINS = CUSTOM_APP_HOSTS.map((host) => `https://${host}`);
